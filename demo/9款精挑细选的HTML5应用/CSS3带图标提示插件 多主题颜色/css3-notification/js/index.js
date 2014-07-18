4ef54eaf9ef1c185593def8f5e6e4a7fac359c8f
creary <1152695512@qq.com>
2014-07-18 12:01:49 +0800

    111111111

--- demo/9款精挑细选的HTML5应用/CSS3带图标提示插件 多主题颜色/css3-notification/js/index.js
+++
@@ -1,5 +0,0 @@
-$(".note__close").click(function() {
-  $(this).parent().fadeOut(500, function() {
-    $(this).remove();
-  });
-});
--- demo/9款精挑细选的HTML5应用/HTML5_CSS3滑块动画菜单 图标动画很酷/html5-css3-fluid-menu-with-icon/js/index.js
+++
@@ -1,9 +0,0 @@
-/*
-Fluid menu with transparent icons.
-----------------------------------------------
-Designed by Vincent Perhirin.
-http://www.behance.net/vincentperhirin
-----------------------------------------------
-https://twitter.com/One_div
-http://one-div.com <-- CSS3 single element database
-*/
